5478e6ad11c54a6e30264691be4c55265d3d9c3e
Brig Bagley <brig@sci.utah.edu>
2014-05-02 12:14:09 -0600

    improving icon for windows

--- fluorender/FluoRender/img/cvt.sh
+++ fluorender/FluoRender/img/cvt.sh
@@ -2,7 +2,7 @@
 
 for file in *.png 
 do
-   png2c.py ${file} > ${file:0:${#file}-4}.h 
+   ./png2c.py ${file} > ${file:0:${#file}-4}.h 
    #rm ${file:0:${#file}-4}
    #echo "${file} : ${file:0:${#file}-4} "
 done
