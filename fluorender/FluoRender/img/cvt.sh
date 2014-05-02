5193c2cc8323abc2b60c01f24b92416a6bc67922
Brig Bagley <brig@sci.utah.edu>
2014-05-02 12:09:48 -0600

    added python script to convert images to headers

--- fluorender/FluoRender/img/cvt.sh
+++ fluorender/FluoRender/img/cvt.sh
@@ -2,7 +2,7 @@
 
 for file in *.png 
 do
-   ~/Downloads/wxWidgets-3.0.0/misc/scripts/png2c.py ${file} > ${file:0:${#file}-4}.h 
+   png2c.py ${file} > ${file:0:${#file}-4}.h 
    #rm ${file:0:${#file}-4}
    #echo "${file} : ${file:0:${#file}-4} "
 done
