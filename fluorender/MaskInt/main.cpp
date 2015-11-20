17b68fa94df1820ae1dede57c27915c9286c450e
basisunus <basisunus@yahoo.com>
2015-11-20 16:58:46 -0700

    adding mask interpolation module

--- fluorender/MaskInt/main.cpp
+++ fluorender/MaskInt/main.cpp
@@ -3,12 +3,119 @@
 #include "Formats/msk_writer.h"
 #include <string>
 #include <codecvt>
+#include <deque>
 //#include <vld.h>
 
-inline std::wstring s2ws(const std::string& utf8) {
-	//    return std::wstring( str.begin(), str.end() );
+typedef struct
+{
+	int x;
+	int y;
+} Pixel;
+
+typedef std::deque<Pixel> PixelList;
+
+inline std::wstring s2ws(const std::string& utf8)
+{
 	std::wstring_convert<std::codecvt_utf8_utf16<wchar_t>, wchar_t> converter;
 	return converter.from_bytes(utf8);
+}
+
+bool GetNeighbor(void *data,
+	int nx, int ny,
+	int x, int y,
+	int dx, int dy,
+	unsigned char &value)
+{
+	int ix = x + dx;
+	if (ix < 0 || ix >= nx)
+		return false;
+	int iy = y + dy;
+	if (iy < 0 || iy >= ny)
+		return false;
+	value = ((unsigned char*)data)[nx*iy+ix];
+	return true;
+}
+
+bool GetPixelList(void* data, int nx, int ny,
+	PixelList &list)
+{
+	unsigned int index;
+	unsigned char value;
+	unsigned char nv;
+	//find the first bounday pixel
+	Pixel sp;
+	sp.x = -1; sp.y = -1;
+	for (int i = 0; i < nx; ++i)
+	for (int j = 0; j < ny; ++j)
+	{
+		index = nx*j + i;
+		value = ((unsigned char*)data)[index];
+		if (!value)
+			continue;
+		//eight directions
+		if (GetNeighbor(data, nx, ny, i, j, -1, -1, nv) &&
+			!nv)
+		{
+			sp.x = i;
+			sp.y = j;
+			break;
+		}
+		if (GetNeighbor(data, nx, ny, i, j, 0, -1, nv) &&
+			!nv)
+		{
+			sp.x = i;
+			sp.y = j;
+			break;
+		}
+		if (GetNeighbor(data, nx, ny, i, j, 1, -1, nv) &&
+			!nv)
+		{
+			sp.x = i;
+			sp.y = j;
+			break;
+		}
+		if (GetNeighbor(data, nx, ny, i, j, -1, 0, nv) &&
+			!nv)
+		{
+			sp.x = i;
+			sp.y = j;
+			break;
+		}
+		if (GetNeighbor(data, nx, ny, i, j, 1, 0, nv) &&
+			!nv)
+		{
+			sp.x = i;
+			sp.y = j;
+			break;
+		}
+		if (GetNeighbor(data, nx, ny, i, j, -1, 1, nv) &&
+			!nv)
+		{
+			sp.x = i;
+			sp.y = j;
+			break;
+		}
+		if (GetNeighbor(data, nx, ny, i, j, 0, 1, nv) &&
+			!nv)
+		{
+			sp.x = i;
+			sp.y = j;
+			break;
+		}
+		if (GetNeighbor(data, nx, ny, i, j, 1, 1, nv) &&
+			!nv)
+		{
+			sp.x = i;
+			sp.y = j;
+			break;
+		}
+	}
+
+	if (sp.x == -1 || sp.y == -1)
+		return false;
+
+	list.push_back(sp);
+	return true;
 }
 
 int main(int argc, char* argv[])
@@ -55,6 +162,18 @@ int main(int argc, char* argv[])
 	int nx = data1->axis[0].size;
 	int ny = data1->axis[1].size;
 	int nz = data1->axis[2].size;
+
+	PixelList list1, list2;
+
+	for (int z = 0; z < nz; ++z)
+	{
+		if (GetPixelList(data1->data, nx, ny, list1) &&
+			GetPixelList(data2->data, nx, ny, list2))
+		{
+			printf("ph\n");
+		}
+
+	}
 
 	return 0;
 }
