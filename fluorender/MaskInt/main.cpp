bb82be745f109498ecbc6f95887c3c94d2840570
basisunus <basisunus@yahoo.com>
2015-11-24 16:50:36 -0700

    mask interpolation finished and tested

--- fluorender/MaskInt/main.cpp
+++ fluorender/MaskInt/main.cpp
@@ -6,6 +6,7 @@
 #include <codecvt>
 #include <deque>
 #include <stack>
+#include <algorithm>
 //#include <vld.h>
 
 typedef struct Pixel_
@@ -97,16 +98,8 @@ bool GetNextPixel(void* data, int nx, int ny,
 	int x, int y, PixelList &list, Pixel &p)
 {
 	//eight neighbors
-	p.x = x - 1; p.y = y - 1;
-	if (BorderPixel(data, nx, ny, x - 1, y - 1) &&
-		!PixelInList(p, list))
-		return true;
 	p.x = x; p.y = y - 1;
 	if (BorderPixel(data, nx, ny, x, y - 1) &&
-		!PixelInList(p, list))
-		return true;
-	p.x = x + 1; p.y = y - 1;
-	if (BorderPixel(data, nx, ny, x + 1, y - 1) &&
 		!PixelInList(p, list))
 		return true;
 	p.x = x - 1; p.y = y;
@@ -117,12 +110,20 @@ bool GetNextPixel(void* data, int nx, int ny,
 	if (BorderPixel(data, nx, ny, x + 1, y) &&
 		!PixelInList(p, list))
 		return true;
-	p.x = x - 1; p.y = y + 1;
-	if (BorderPixel(data, nx, ny, x - 1, y + 1) &&
-		!PixelInList(p, list))
-		return true;
 	p.x = x; p.y = y + 1;
 	if (BorderPixel(data, nx, ny, x, y + 1) &&
+		!PixelInList(p, list))
+		return true;
+	p.x = x - 1; p.y = y - 1;
+	if (BorderPixel(data, nx, ny, x - 1, y - 1) &&
+		!PixelInList(p, list))
+		return true;
+	p.x = x + 1; p.y = y - 1;
+	if (BorderPixel(data, nx, ny, x + 1, y - 1) &&
+		!PixelInList(p, list))
+		return true;
+	p.x = x - 1; p.y = y + 1;
+	if (BorderPixel(data, nx, ny, x - 1, y + 1) &&
 		!PixelInList(p, list))
 		return true;
 	p.x = x + 1; p.y = y + 1;
@@ -191,6 +192,72 @@ bool GetPixelList(void* data, int nx, int ny,
 	return true;
 }
 
+void ReverseList(PixelList& list)
+{
+	std::reverse(list.begin(), list.end());
+}
+
+bool ReorderLists(PixelList& list1, PixelList& list2)
+{
+	int flag1[6] = { 0, 0, 0, 0, 0, 0 };
+
+	Pixel p1, p2;
+	int dx, dy;
+	for (size_t i = 0; i < list1.size() - 1; ++i)
+	{
+		p1 = list1[i];
+		p2 = list1[i + 1];
+
+		dx = p2.x - p1.x;
+		dy = p2.y - p1.y;
+		if (dx > 0)
+			flag1[0]++;
+		else if (dx < 0)
+			flag1[1]++;
+		else
+			flag1[2]++;
+		if (dy > 0)
+			flag1[3]++;
+		else if (dy < 0)
+			flag1[4]++;
+		else
+			flag1[5]++;
+	}
+
+	int flag2[6] = { 0,0,0,0,0,0 };
+
+	for (size_t i = 0; i < list2.size() - 1; ++i)
+	{
+		p1 = list2[i];
+		p2 = list2[i + 1];
+
+		dx = p2.x - p1.x;
+		dy = p2.y - p1.y;
+		if (dx > 0)
+			flag2[0]++;
+		else if (dx < 0)
+			flag2[1]++;
+		else
+			flag2[2]++;
+		if (dy > 0)
+			flag2[3]++;
+		else if (dy < 0)
+			flag2[4]++;
+		else
+			flag2[5]++;
+	}
+
+	double diff = 0;
+	for (size_t i = 0; i < 6; ++i)
+		diff += fabs(double(flag1[i]) / double(list1.size()) -
+			double(flag2[i]) / double(list2.size()));
+
+	if (diff > 1.5)
+		ReverseList(list1);
+
+	return true;
+}
+
 bool SearchBorder(void* page, int nx, int ny,
 	int x, int y, int ix, int iy, Pixel& fp)
 {
@@ -237,44 +304,59 @@ bool SearchBorder(void* page, int nx, int ny,
 	return false;
 }
 
-bool GetFillPixel(void* data1, void* data2,
+bool GetFillPixel(void* data1, void* data2, void* data_out,
 	int nx, int ny, int z, Pixel& fp)
 {
 	unsigned char* page1 = ((unsigned char*)data1) + nx*ny*z;
 	unsigned char* page2 = ((unsigned char*)data2) + nx*ny*z;
+	unsigned char* page_out = ((unsigned char*)data_out) + nx*ny*z;
 
 	Pixel p1, p2;
+	bool result = false;
 	//top
-	if (SearchBorder(page1, nx, ny, 0, 0, 1, 0, p1) &&
-		SearchBorder(page2, nx, ny, 0, 0, 1, 0, p2))
-	{
-		fp.x = (p1.x + p2.x) / 2;
-		fp.y = (p1.y + p2.y) / 2;
-		return true;
-	}
+	result = SearchBorder(page1, nx, ny, 0, 0, 1, 0, p1) &&
+		SearchBorder(page2, nx, ny, 0, 0, 1, 0, p2);
 	//bottom
-	if (SearchBorder(page1, nx, ny, 0, ny-1, 1, 0, p1) &&
-		SearchBorder(page2, nx, ny, 0, ny - 1, 1, 0, p2))
-	{
-		fp.x = (p1.x + p2.x) / 2;
-		fp.y = (p1.y + p2.y) / 2;
-		return true;
-	}
+	if (!result)
+		result = SearchBorder(page1, nx, ny, 0, ny - 1, 1, 0, p1) &&
+		SearchBorder(page2, nx, ny, 0, ny - 1, 1, 0, p2);
 	//left
-	if (SearchBorder(page1, nx, ny, 0, 0, 0, 1, p1) &&
-		SearchBorder(page2, nx, ny, 0, 0, 0, 1, p2))
-	{
-		fp.x = (p1.x + p2.x) / 2;
-		fp.y = (p1.y + p2.y) / 2;
-		return true;
-	}
+	if (!result)
+		result = SearchBorder(page1, nx, ny, 0, 0, 0, 1, p1) &&
+		SearchBorder(page2, nx, ny, 0, 0, 0, 1, p2);
 	//right
-	if (SearchBorder(page1, nx, ny, nx-1, 0, 0, 1, p1) &&
-		SearchBorder(page2, nx, ny, nx-1, 0, 0, 1, p2))
+	if (!result)
+		result = SearchBorder(page1, nx, ny, nx - 1, 0, 0, 1, p1) &&
+		SearchBorder(page2, nx, ny, nx - 1, 0, 0, 1, p2);
+
+	if (result)
 	{
 		fp.x = (p1.x + p2.x) / 2;
 		fp.y = (p1.y + p2.y) / 2;
-		return true;
+		unsigned char value;
+		if (GetNeighbor(data_out, nx, ny, fp.x, fp.y, 0, 0, value) && !value)
+			return true;
+		if (GetNeighbor(data_out, nx, ny, fp.x, fp.y, -1, 0, value) && !value)
+		{
+			fp.x -= 1;
+			return true;
+		}
+		if (GetNeighbor(data_out, nx, ny, fp.x, fp.y, 1, 0, value) && !value)
+		{
+			fp.x += 1;
+			return true;
+		}
+		if (GetNeighbor(data_out, nx, ny, fp.x, fp.y, 0, -1, value) && !value)
+		{
+			fp.y -= 1;
+			return true;
+		}
+		if (GetNeighbor(data_out, nx, ny, fp.x, fp.y, 0, 1, value) && !value)
+		{
+			fp.y += 1;
+			return true;
+		}
+		return false;
 	}
 
 	return false;
@@ -315,9 +397,10 @@ Nrrd* NewData(int nx, int ny, int nz, int bits,
 	return result;
 }
 
-void ZeroData(void* data, int nx, int ny, int nz, int bits)
+void ZeroData(void* data, int nx, int ny, int z, int bits)
 {
-	memset(data, 0, nx*ny*nz*(bits / 8));
+	unsigned char* page = ((unsigned char*)data) + z*nx*ny;
+	memset(page, 0, nx*ny*(bits / 8));
 }
 
 void WriteList(void* data, int nx, int ny, int z, PixelList& list)
@@ -379,14 +462,154 @@ void FillData(void* data, int nx, int ny, int z, Pixel& fp)
 	}
 }
 
-void Interpolate(PixelList& list1,
-	PixelList& list2, PixelList& list_out,
-	double t)
+bool BoundPixel(int nx, int ny, Pixel& p)
 {
-	if (list1.size() < list2.size())
-		Interpolate(list2, list1, list_out, 1.0 - t);
+	return (p.x == 0 || p.x == nx - 1 ||
+		p.y == 0 || p.y == ny - 1);
+}
 
+bool NeighborPixels(Pixel& p1, Pixel& p2)
+{
+	return (abs(p1.x - p2.x) <= 1 &&
+		abs(p1.y - p2.y) <= 1);
+}
+
+void FillGap(Pixel& p1, Pixel& p2, PixelList& list)
+{
+	int dx = p2.x - p1.x;
+	int dy = p2.y - p1.y;
+
+	int qt, incx, incy;
+	incx = dx > 0? 1: -1;
+	incy = dy > 0? 1: -1;
+	Pixel p;
+
+	if (abs(dx) > abs(dy))
+	{
+		if (abs(dy))
+			qt = (abs(dx) + 1) / (abs(dy) + 1);
+		else
+			qt = 0;
+		p.x = p1.x; p.y = p1.y;
+		for (int i = 0; i < abs(dx) - 1; ++i)
+		{
+			p.x += incx;
+			if (qt && i%qt == 0 && p.y != p2.y)
+				p.y += incy;
+			list.push_back(p);
+		}
+	}
+	else
+	{
+		if (abs(dx))
+			qt = (abs(dy) + 1) / (abs(dx) + 1);
+		else
+			qt = 0;
+		p.x = p1.x; p.y = p1.y;
+		for (int i = 0; i < abs(dy) - 1; ++i)
+		{
+			p.y += incy;
+			if (qt && i%qt == 0 && p.x != p2.x)
+				p.x += incx;
+			list.push_back(p);
+		}
+	}
+}
+
+void BoundIntrp(int nx, int ny, double t, Pixel& p1, Pixel& p2, Pixel& p_out)
+{
+	PixelList corners;
+	Pixel p;
+	p.x = 0; p.y = 0;
+	corners.push_back(p);
+	p.x = nx-1; p.y = 0;
+	corners.push_back(p);
+	p.x = nx-1; p.y = ny-1;
+	corners.push_back(p);
+	p.x = 0; p.y = ny-1;
+	corners.push_back(p);
+
+	int start_corner;
+	int mind = (nx + ny) * 2;
+	int pd;
+	for (size_t i = 0; i < corners.size(); ++i)
+	{
+		pd = abs(p1.x - corners[i].x) + abs(p1.y - corners[i].y);
+		if (pd < mind)
+		{
+			mind = pd;
+			start_corner = i;
+		}
+	}
+
+	int accd = 0;
+	int id = start_corner;
+	int ppd;
+	int corner[4] = {-1, -1, -1, -1};
+	int ccnt = 0;
+
+	p = p1;
+	for (size_t i = 0; i < corners.size(); ++i)
+	{
+		pd = abs(p.x - corners[id].x) + abs(p.y - corners[id].y);
+		ppd = abs(p.x - p2.x) + abs(p.y - p2.y);
+		if (pd < ppd)
+		{
+			p = corners[id];
+			accd += pd;
+			corner[i] = id;
+			ccnt++;
+			if (id == corners.size() - 1)
+				id = 0;
+			else
+				id++;
+		}
+		else
+		{
+			accd += ppd;
+			break;
+		}
+	}
+
+	accd = int(t * accd);
+
+	if (ccnt == 0)
+	{
+		p_out.x = p1.x + int((p2.x - p1.x) * t + 0.5);
+		p_out.y = p1.y + int((p2.y - p1.y) * t + 0.5);
+		return;
+	}
+
+	p = p1;
+	for (size_t i = 0; i < ccnt; ++i)
+	{
+		pd = abs(p.x - corners[corner[i]].x) + abs(p.y - corners[corner[i]].y);
+		if (pd == 0)
+			continue;
+		if (accd <= pd)
+		{
+			p_out.x = p.x + int((corners[corner[i]].x - p.x) * double(accd) / double(pd) + 0.5);
+			p_out.y = p.y + int((corners[corner[i]].y - p.y) * double(accd) / double(pd) + 0.5);
+			return;
+		}
+		else
+		{
+			p = corners[corner[i]];
+			accd -= pd;
+		}
+	}
+	pd = abs(p2.x - p.x) + abs(p2.y - p.y);
+	p_out.x = p.x + int((p2.x - p.x) * double(accd) / double(pd) + 0.5);
+	p_out.y = p.y + int((p2.y - p.y) * double(accd) / double(pd) + 0.5);
+
+}
+
+void Interpolate(int nx, int ny,
+	PixelList& list1, PixelList& list2,
+	PixelList& list_out, double t)
+{
 	Pixel p1, p2, p_out;
+	PixelList list;
 	int i2;
 	for (size_t i = 0; i < list1.size(); ++i)
 	{
@@ -395,7 +618,52 @@ void Interpolate(PixelList& list1,
 		p2 = list2[i2];
 		p_out.x = p1.x + int((p2.x - p1.x) * t + 0.5);
 		p_out.y = p1.y + int((p2.y - p1.y) * t + 0.5);
-		list_out.push_back(p_out);
+		list.push_back(p_out);
+	}
+
+	//check and fill gaps
+	Pixel pn;
+	for (size_t i = 0; i < list.size(); ++i)
+	{
+		p_out = list[i];
+		if ((i == 0 || i == list.size() - 1)
+			&& !BoundPixel(nx, ny, p_out))
+		{
+			if (i == 0)
+			{
+				p1 = list1[0];
+				p2 = list2[0];
+			}
+			else
+			{
+				p1 = list1[list1.size() - 1];
+				p2 = list2[list2.size() - 1];
+			}
+			//find out pn
+			BoundIntrp(nx, ny, t, p1, p2, pn);
+			//fill gap
+			if (i == 0)
+			{
+				list_out.push_back(pn);
+				FillGap(pn, p_out, list_out);
+				list_out.push_back(p_out);
+			}
+			if (i == list.size() - 1)
+			{
+				list_out.push_back(p_out);
+				FillGap(p_out, pn, list_out);
+				list_out.push_back(pn);
+			}
+		}
+		else
+			list_out.push_back(p_out);
+
+		if (i < list.size() - 1)
+		{
+			pn = list[i + 1];
+			if (!NeighborPixels(p_out, pn))
+				FillGap(p_out, pn, list_out);
+		}
 	}
 }
 
@@ -456,13 +724,23 @@ int main(int argc, char* argv[])
 		list_out.clear();
 
 		if (GetPixelList(data1->data, nx, ny, z, list1) &&
-			GetPixelList(data2->data, nx, ny, z, list2))
+			GetPixelList(data2->data, nx, ny, z, list2) &&
+			ReorderLists(list1, list2))
 		{
-			Interpolate(list1, list2, list_out, int_t);
+			if (list1.size() > list2.size())
+				Interpolate(nx, ny, list2, list1, list_out, 1.0 - int_t);
+			else
+				Interpolate(nx, ny, list1, list2, list_out, int_t);
 			WriteList(data_out->data, nx, ny, z, list_out);
 			Pixel fp;
-			if (GetFillPixel(data1->data, data2->data, nx, ny, z, fp))
+			if (GetFillPixel(data1->data, data2->data, data_out->data, nx, ny, z, fp))
 				FillData(data_out->data, nx, ny, z, fp);
+
+			////test
+			//ZeroData(data1->data, nx, ny, z, 8);
+			//WriteList(data1->data, nx, ny, z, list1);
+			//ZeroData(data2->data, nx, ny, z, 8);
+			//WriteList(data2->data, nx, ny, z, list2);
 		}
 	}
 
@@ -471,6 +749,19 @@ int main(int argc, char* argv[])
 	writer.SetSpacings(spcx, spcy, spcz);
 	writer.SetCompression(false);
 	writer.Save(outfile.c_str(), 0);
+
+	////test
+	//std::wstring testfile;
+	//std::wstring path;
+	//int64_t pos = outfile.find_last_of(L'\\');
+	//if (pos != -1)
+	//	path = outfile.substr(0, pos + 1);
+	//writer.SetData(data1);
+	//testfile = path + L"test1.tif";
+	//writer.Save(testfile.c_str(), 0);
+	//writer.SetData(data2);
+	//testfile = path + L"test2.tif";
+	//writer.Save(testfile.c_str(), 0);
 
 	printf("All done. Quit.\n");
 
