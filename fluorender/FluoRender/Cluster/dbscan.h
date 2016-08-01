7b5cdba3701646e8493a12c4d86ae32c703f727c
basisunus <basisunus@yahoo.com>
2016-08-01 16:56:10 -0600

    adding dbscan algorithm

--- fluorender/FluoRender/Cluster/dbscan.h
+++ fluorender/FluoRender/Cluster/dbscan.h
@@ -25,94 +25,69 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
 FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
 DEALINGS IN THE SOFTWARE.
 */
-#ifndef FL_CompAnalyzer_h
-#define FL_CompAnalyzer_h
+#ifndef FL_Dbscan_h
+#define FL_Dbscan_h
 
 #include <FLIVR/Point.h>
-#include <FLIVR/Color.h>
-#include <string>
-#include <list>
 #include <boost/unordered_map.hpp>
-
-class VolumeData;
-class Annotations;
+#include <vector>
 
 namespace FL
 {
-	class ComponentAnalyzer
+	struct ClusterPoint
+	{
+		unsigned int id;
+		bool visited;
+		FLIVR::Point center;
+		float intensity;
+	};
+
+	inline float Dist(const ClusterPoint &p1, const ClusterPoint &p2, float w)
+	{
+		FLIVR::Vector p1p2 = p1.center - p2.center;
+		float int_diff = fabs(p1.intensity - p2.intensity);
+		return p1p2.length() + w * int_diff;
+	}
+
+	typedef boost::unordered_map<unsigned int, ClusterPoint> Cluster;
+	typedef Cluster::iterator ClusterIter;
+
+	class ClusterSet
 	{
 	public:
-		ComponentAnalyzer(VolumeData* vd=0);
-		~ComponentAnalyzer();
-
-		void SetVolume(VolumeData* vd)
-		{
-			m_comp_list_dirty = m_vd != vd;
-			m_vd = vd;
-		}
-		VolumeData* GetVolume()
-		{ return m_vd; }
-
-		void Analyze(bool sel);
-		void OutputFormHeader(std::string &str);
-		//print out results
-		//verbose: 0-clean output; 1-extra info
-		void OutputCompList(std::string &str, int verbose, std::string comp_header="");
-		bool GenAnnotations(Annotations &ann);
-		//color_type: 1-id-based; 2-size-based
-		bool GenMultiChannels(std::list<VolumeData*> &channs, int color_type);
-		bool GenRgbChannels(std::list<VolumeData*> &channs, int color_type);
+		ClusterSet() {};
+		~ClusterSet() {};
 
 	private:
-		VolumeData* m_vd;
-		struct CompInfo
-		{
-			unsigned int id;
-			unsigned int sumi;
-			double sumd;
-			unsigned int ext_sumi;
-			double ext_sumd;
-			double mean;
-			double var;
-			double m2;
-			double min;
-			double max;
-			FLIVR::Point pos;
+		std::vector<Cluster> m_list;
+	};
 
-			CompInfo() {}
-			CompInfo(unsigned int _id)
-				:id(_id)
-			{}
+	class ClusterDbscan
+	{
+	public:
+		ClusterDbscan();
+		~ClusterDbscan();
 
-			bool operator<(const CompInfo &info2) const
-			{
-				return id < info2.id;
-			}
-			bool operator==(const CompInfo &info2) const
-			{
-				return id == info2.id;
-			}
-		};
-		typedef boost::unordered_map<unsigned int, CompInfo> CompUList;
-		typedef CompUList::iterator CompUListIter;
-		class CompList : public std::list<CompInfo>
-		{
-		public:
-			unsigned int min;
-			unsigned int max;
-		};
-		typedef CompList::iterator CompListIter;
-
-		CompList m_comp_list;
-		bool m_comp_list_dirty;
+		void SetData(Cluster &data)
+		{ m_data = data; }
+		ClusterSet &GetResult()
+		{ return m_result; }
+		void SetSize(unsigned int size)
+		{ m_size = size; }
+		void SetEps(float eps)
+		{ m_eps = eps; }
+		bool Execute();
 
 	private:
-		unsigned int GetExt(unsigned int* data_label,
-			unsigned long long index,
-			unsigned int id,
-			int nx, int ny, int nz,
-			int i, int j, int k);
-		FLIVR::Color GetColor(CompInfo &comp_info, VolumeData* vd, int color_type);
+		Cluster m_data;
+		ClusterSet m_result;
+		unsigned int m_size;
+		float m_eps;
+		float m_intw;
+
+	private:
+		void ExpandCluster();
+		Cluster GetNeighbors(ClusterPoint &p);
 	};
 }
-#endif//FL_CompAnalyzer_h
+#endif//FL_Dbscan_h
