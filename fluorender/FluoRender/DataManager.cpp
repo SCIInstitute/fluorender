f9cc775cd8ac861f6fe79133826838c51fe55554
basisunus <wanyong@cs.utah.edu>
2018-04-25 11:52:23 -0600

    formatting

--- fluorender/FluoRender/DataManager.cpp
+++ fluorender/FluoRender/DataManager.cpp
@@ -4531,17 +4531,17 @@ wxString DataManager::SearchProjectPath(wxString &filename)
 	if (m_prj_path == "")
 		return "";
 	wxString search_str;
-	for (i=pathname.Length()-1; i>=0; i--)
+	for (i = pathname.Length() - 1; i >= 0; i--)
 	{
-		if (pathname[i]=='\\' || pathname[i]=='/')
+		if (pathname[i] == '\\' || pathname[i] == '/')
 		{
-            search_str.Prepend(GETSLASH());
+			search_str.Prepend(GETSLASH());
 			wxString name_temp = m_prj_path + search_str;
 			if (wxFileExists(name_temp))
 				return name_temp;
 		}
-        else
-            search_str.Prepend(pathname[i]);
+		else
+			search_str.Prepend(pathname[i]);
 	}
 	return "";
 }
