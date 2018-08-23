e7510aab01f5b734c2e8603f7e7d607cb0528d27
basisunus <wanyong@cs.utah.edu>
2018-08-23 16:56:25 -0600

    added objectfactory test

--- fluorender/Tester/ObjectFactoryTest.cpp
+++ fluorender/Tester/ObjectFactoryTest.cpp
@@ -18,4 +18,13 @@ void FactoryTest()
 	}
 
 	ASSERT_EQ(num, factory->getNum());
+
+	for (int i = 0; i < num; ++i)
+	{
+		Object* obj = factory->get(i);
+		unsigned int id = i + 1;
+		string name = "object" + to_string(id);
+		ASSERT_EQ(id, obj->getId());
+		ASSERT_EQ(name, obj->getName());
+	}
 }
