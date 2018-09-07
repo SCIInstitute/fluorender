d2e69e49809f43f83602bf291ff30ddb0226f189
remal <technoremal@gmail.com>
2018-09-07 13:53:15 -0600

    [Fluorender] Updating the java code for physical metadata.

--- Java_Code/ImageJ_Reader/src/main/java/ImageJ_Reader.java
+++ Java_Code/ImageJ_Reader/src/main/java/ImageJ_Reader.java
@@ -1,5 +1,6 @@
 import ij.process.ImageProcessor;
 import ij.process.LUT;
+import loci.common.services.ServiceFactory;
 import loci.formats.ChannelSeparator;
 import loci.formats.IFormatReader;
 import ij.ImagePlus;
@@ -16,15 +17,21 @@ import ij.IJ;
 import javax.imageio.ImageIO;
 
 import loci.formats.FormatException;
+import loci.formats.ImageReader;
+import loci.formats.meta.IMetadata;
+import loci.formats.services.OMEXMLService;
 import loci.plugins.util.ImageProcessorReader;
 import loci.plugins.util.LociPrefs;
+import ome.units.quantity.Length;
+import ome.units.quantity.Time;
 
 //TODO: Add threads to load the images fast.
 public class ImageJ_Reader {
     public static void main(String[] args) {
-        //short[] test = getIntDataB(args, 0, 1);
+        short[] test = getIntDataB(args, 0, 1);
         int[] test2 = getMetaData(args);
-        byte[] test3 = getByteData(args, 0, 0);
+        //byte[] test3 = getByteData(args, 0, 0);
+        //byte[] test3 = getIntDataB(args, 0, 0);
     }
 
     private static ImagePlus applyLookupTables(IFormatReader r, ImagePlus imp, byte[][][] lookupTable) {
@@ -43,15 +50,12 @@ public class ImageJ_Reader {
         }
         return imp;
     }
-
-    //The method for returning number of z channel images.
-    public static int[] getMetaData(String[] args) {
-        int[] dump = new int[1];
-        dump[0] = -1;
+/*
+    public static int[] getMetaDataN(String[] args) {
         String id = args[0];
-        ImageProcessorReader ip_reader = new ImageProcessorReader(new ChannelSeparator(LociPrefs.makeImageReader()));
+        //ImageProcessorReader ip_reader = new ImageProcessorReader(new ChannelSeparator(LociPrefs.makeImageReader()));
+        ImageReader ip_reader = new ImageReader();
         try {
-            //IJ.showStatus("Examining file " + name);
             ip_reader.setId(id);
 
             int[] metadata = new int[7];
@@ -62,14 +66,94 @@ public class ImageJ_Reader {
             metadata[4] = ip_reader.getSizeC();
             metadata[5] = ip_reader.getSizeT();
             metadata[6] = ip_reader.getPixelType();
-            int test = ip_reader.getPixelType();
+
+            long totalsize = (long)metadata[1] * (long)metadata[2] * (long)metadata[3];
+            if (totalsize > Integer.MAX_VALUE){
+                throw new Exception("Array size too big");
+            }
+            //int test = ip_reader.getPixelType();
             return metadata;
         } catch (FormatException exc) {
-            dump[0] = -2;
+            int[] test = new int[1];
+            test[0] = 4;
+            return test;
         } catch (IOException exc) {
-            dump[0] = -3;
+            int[] test = new int[1];
+            test[0] = 5;
+            return test;
+        } catch (Exception exc){
+            int[] test = new int[1];
+            test[0] = 6;
+            System.out.println(exc.getMessage());
+            if (exc.getMessage() == "Array size too big"){
+                test[0] = 7;
+            }
+            return test;
         }
-        return dump;
+    }
+*/
+
+    public static int[] getMetaData(String[] args) {
+        String id = args[0];
+        ImageProcessorReader ip_reader = new ImageProcessorReader(new ChannelSeparator(LociPrefs.makeImageReader()));
+        try {
+            // Setting up xml reading.
+            ServiceFactory factory = new ServiceFactory();
+            OMEXMLService service = factory.getInstance(OMEXMLService.class);
+            IMetadata meta = service.createOMEXMLMetadata();
+            ip_reader.setMetadataStore(meta);
+
+            ip_reader.setId(id);
+
+            // Reading the pixel level matadata.
+            int[] metadata = new int[11];
+            metadata[0] = ip_reader.getImageCount();
+            metadata[1] = ip_reader.getSizeX();
+            metadata[2] = ip_reader.getSizeY();
+            metadata[3] = ip_reader.getSizeZ();
+            metadata[4] = ip_reader.getSizeC();
+            metadata[5] = ip_reader.getSizeT();
+            metadata[6] = ip_reader.getPixelType();
+
+            long totalsize = (long)metadata[1] * (long)metadata[2] * (long)metadata[3];
+            if (totalsize > Integer.MAX_VALUE){
+                throw new Exception("Array size too big");
+            }
+
+            // Reading the physical metadata. TODO: I dont know what this 0 is for ? :(. Documentation is not helpful.
+            Length physicalSizeX = meta.getPixelsPhysicalSizeX(0);
+            Length physicalSizeY = meta.getPixelsPhysicalSizeY(0);
+            Length physicalSizeZ = meta.getPixelsPhysicalSizeZ(0);
+            Time timeIncrement = meta.getPixelsTimeIncrement(0);
+
+            int px = (int)(physicalSizeX.value().doubleValue() * 10000);
+            int py = (int)(physicalSizeY.value().doubleValue() * 10000);
+            int pz = (int)(physicalSizeZ.value().doubleValue() * 10000);
+            int pt = (int)(timeIncrement.value().doubleValue() * 10000);
+
+            metadata[7] = px;
+            metadata[8] = py;
+            metadata[9] = pz;
+            metadata[10] = pt;
+
+            return metadata;
+        } catch (FormatException exc) {
+            int[] test = new int[1];
+            test[0] = 4;
+            return test;
+        } catch (IOException exc) {
+            int[] test = new int[1];
+            test[0] = 5;
+            return test;
+        } catch (Exception exc){
+            int[] test = new int[1];
+            test[0] = 6;
+            System.out.println(exc.getMessage());
+            if (exc.getMessage() == "Array size too big"){
+                test[0] = 7;
+            }
+            return test;
+        }
     }
 
     public static int[] getIntDataS(String[] args) {
@@ -87,10 +171,73 @@ public class ImageJ_Reader {
         return test;
     }
 
+/*
+    public static byte[] getIntDataB(String[] args, int time_id, int channel_id) {
+        String id = args[0];
+        //ImageProcessorReader ip_reader = new ImageProcessorReader(new ChannelSeparator(LociPrefs.makeImageReader()));
+        ImageReader ip_reader = new ImageReader();
+        try {
+            ip_reader.setId(id);
+            int num = ip_reader.getImageCount();
+            int width = ip_reader.getSizeX();
+            int height = ip_reader.getSizeY();
+            int channels = ip_reader.getSizeC();
+            int depth = ip_reader.getSizeZ();
+            int time_seq = ip_reader.getSizeT();
+
+            ImageStack stack = new ImageStack(width, height);
+            byte[][][] lookupTable = new byte[ip_reader.getSizeC()][][];
+
+            // Adding all the slices into a 2D array and returning those values.
+            int time_step = channels * depth;
+            //int offset = time_id * time_step + depth_id * channels;
+            int time_offset = time_id * time_step;
+
+            byte[] test_array = new byte[width * height * depth];
+
+            int pixel_offset = 0;
+            for (int d = 0; d < depth; ++d){
+                pixel_offset = d * width * height;
+                int depth_offset = time_offset + (d * channels);
+
+                byte[] slice = ip_reader.openBytes(d); //A slice contains n channels.
+                for (int h = 0; h < height; h++) {
+                    for (int w = 0; w < width; w++) {
+                        test_array[pixel_offset + (h * width) + w] = slice[(channel_id*height*width) + (h*width) + w];
+                    }
+                }
+
+//                ImageProcessor ip = ip_reader.openProcessors(depth_offset+channel_id)[0];
+//                int[][] temp_array = ip.getIntArray();
+//                for (int h = 0; h < height; h++) {
+//                    for (int w = 0; w < width; w++) {
+//                        test_array[pixel_offset + (h * width) + w] = (short)temp_array[w][h];
+//                    }
+//                }
+                //java.awt.image.BufferedImage awt_Ip = ip.getBufferedImage();
+                //ImageIO.write(awt_Ip, "jpg", new File("D:\\Dev_Environment\\Test_Files\\Test_Folder\\outNB" + Integer.toString(depth_offset) + ".jpg"));
+            }
+            return test_array;
+        } catch (FormatException exc) {
+            byte[] test = new byte[1];
+            test[0] = 1;
+            return test;
+        } catch (IOException exc) {
+            byte[] test = new byte[1];
+            test[0] = 2;
+            return test;
+        } catch (Exception exc){
+            byte[] test = new byte[1];
+            test[0] = 3;
+            return test;
+        }
+    }
+*/
+
+
     public static short[] getIntDataB(String[] args, int time_id, int channel_id) {
         String id = args[0];
         ImageProcessorReader ip_reader = new ImageProcessorReader(new ChannelSeparator(LociPrefs.makeImageReader()));
-
         try {
             ip_reader.setId(id);
             int num = ip_reader.getImageCount();
@@ -135,7 +282,9 @@ public class ImageJ_Reader {
             test[0] = 2;
             return test;
         } catch (Exception exc){
-            return null;
+            short[] test = new short[1];
+            test[0] = 3;
+            return test;
         }
     }
 
