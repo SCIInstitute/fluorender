e60c98e46afa67f7b7e6b5714be3d40bbc2a8a02
Sailanarmo <rlramirez12@gmail.com>
2019-09-16 15:51:53 -0600

    The GUI now launches. The next step will be to move over Wx widget's gui to a QT Gui.

---
+++ Base/FUI/controller.hpp
@@ -0,0 +1,67 @@
+#ifndef CONTROLLER_HPP
+#define CONTROLLER_HPP
+
+#include <functional>
+#include <vector>
+#include <iostream>
+#include <utility>
+
+/*
+ * This templated class takes a reference of classes in any variety. It uses
+ * variadic templates which has been hard to understand. However, it just means
+ * I can accept any number of classes as an argument.
+ *
+ * From the constructor, it creates a tuple of classes by reference. From there,
+ * it waits until the funtion setValues or getValues is called. It then does
+ * crazy lambda magic, unpacks the variadic template, and applies the function
+ * for each class.
+ *
+ * TODO: Change x to Class
+ */
+template<typename...Classes>
+class Controller
+{
+  public:
+    Controller(Classes&...objects) : objects(objects...){}
+    Controller(std::tuple<Classes&...> tup) : objects(tup){}
+
+    void setValues(int value)
+    {
+      std::apply([&](auto&...x) { (x.updateValue(value),...);}, objects);
+    }
+
+    void getValues(std::vector<int> &values) const
+    {
+      std::apply([&](auto&...x) { (values.push_back(x.get()),...);}, objects);
+    }
+
+    std::tuple<Classes&...> getObjects() const
+    {
+      return objects;
+    }
+
+  private:
+    std::tuple<Classes&...> objects;
+};
+
+template<typename...ClassesL, typename...ClassesR>
+auto operator+(const Controller<ClassesL...>& lhs, const Controller<ClassesR...>& rhs)
+{
+  return Controller(std::tuple_cat(lhs.getObjects(),rhs.getObjects()));
+}
+
+template<typename...ClassesL, typename ClassesR>
+auto operator+(const Controller<ClassesL...> &lhs, ClassesR rhs)
+{
+  Controller<ClassesR> makeController(rhs);
+  return Controller(std::tuple_cat(lhs.getObjects(),makeController.getObjects()));
+}
+
+template<typename ClassesL, typename...ClassesR>
+auto operator+(ClassesL lhs, const Controller<ClassesR...> &rhs)
+{
+  Controller<ClassesL> makeController(lhs);
+  return Controller(std::tuple_cat(makeController.getObjects(),rhs.getObjects()));
+}
+
+#endif // CONTROLLER_HPP
