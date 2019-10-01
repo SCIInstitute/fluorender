7d537f16834cb3e6eba5e749e02734fdab4d8f2a
basisunus <basisunus@yahoo.com>
2019-10-01 12:34:04 -0600

    added vr support

---
+++ fluorender/XInput/XboxController.cpp
@@ -0,0 +1,79 @@
+/*
+For more information, please see: http://software.sci.utah.edu
+
+The MIT License
+
+Copyright (c) 2019 Scientific Computing and Imaging Institute,
+University of Utah.
+
+
+Permission is hereby granted, free of charge, to any person obtaining a
+copy of this software and associated documentation files (the "Software"),
+to deal in the Software without restriction, including without limitation
+the rights to use, copy, modify, merge, publish, distribute, sublicense,
+and/or sell copies of the Software, and to permit persons to whom the
+Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included
+in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+DEALINGS IN THE SOFTWARE.
+*/
+#include "XboxController.h"
+
+XboxController::XboxController(int playerNumber)
+{
+	// Set the Controller Number
+	_controllerNum = playerNumber - 1;
+}
+
+XINPUT_STATE XboxController::GetState()
+{
+	// Zeroise the state
+	ZeroMemory(&_controllerState, sizeof(XINPUT_STATE));
+
+	// Get the state
+	XInputGetState(_controllerNum, &_controllerState);
+
+	return _controllerState;
+}
+
+bool XboxController::IsConnected()
+{
+	// Zeroise the state
+	ZeroMemory(&_controllerState, sizeof(XINPUT_STATE));
+
+	// Get the state
+	DWORD Result = XInputGetState(_controllerNum, &_controllerState);
+
+	if(Result == ERROR_SUCCESS)
+	{
+		return true;
+	}
+	else
+	{
+		return false;
+	}
+}
+
+void XboxController::Vibrate(int leftVal, int rightVal)
+{
+	// Create a Vibraton State
+	XINPUT_VIBRATION Vibration;
+
+	// Zeroise the Vibration
+	ZeroMemory(&Vibration, sizeof(XINPUT_VIBRATION));
+
+	// Set the Vibration Values
+	Vibration.wLeftMotorSpeed = leftVal;
+	Vibration.wRightMotorSpeed = rightVal;
+
+	// Vibrate the controller
+	XInputSetState(_controllerNum, &Vibration);
+}
