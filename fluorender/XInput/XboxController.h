41ecaa7358db9570a4e47de5dc452d53d4d9c637
basisunus <basisunus@yahoo.com>
2019-10-01 12:34:04 -0600

    added vr support

---
+++ fluorender/XInput/XboxController.h
@@ -0,0 +1,55 @@
+/*
+For more information, please see: http://software.sci.utah.edu
+
+The MIT License
+
+Copyright (c) 2019 Scientific Computing and Imaging Institute,
+University of Utah.
+
+
+Permission is hereby granted, free of charge, to any person obtaining a
+copy of this software and associated documentation files (the "Software"),
+to deal in the Software without restriction, including without limitation
+the rights to use, copy, modify, merge, publish, distribute, sublicense,
+and/or sell copies of the Software, and to permit persons to whom the
+Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included
+in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+DEALINGS IN THE SOFTWARE.
+*/
+#ifndef _XBOX_CONTROLLER_H_
+#define _XBOX_CONTROLLER_H_
+
+// No MFC
+#define WIN32_LEAN_AND_MEAN
+
+// We need the Windows Header and the XInput Header
+#include <windows.h>
+#include <XInput.h>
+
+// Now, the XInput Library
+// NOTE: COMMENT THIS OUT IF YOU ARE NOT USING A COMPILER THAT SUPPORTS THIS METHOD OF LINKING LIBRARIES
+//#pragma comment(lib, "XInput.lib")
+
+// XBOX Controller Class Definition
+class XboxController
+{
+private:
+	XINPUT_STATE _controllerState;
+	int _controllerNum;
+public:
+	XboxController(int playerNumber);
+	XINPUT_STATE GetState();
+	bool IsConnected();
+	void Vibrate(int leftVal = 0, int rightVal = 0);
+};
+
+#endif//_XBOX_CONTROLLER_H_
