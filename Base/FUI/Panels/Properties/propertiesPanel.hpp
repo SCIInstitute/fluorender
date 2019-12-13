ec569265abb9df3587893f952ee24b4c6a3a1c96
Sailanarmo <rlramirez12@gmail.com>
2019-12-13 14:28:53 -0700

    Added a to-do. Ready to merge with main branch.

--- Base/FUI/Panels/Properties/propertiesPanel.hpp
+++ Base/FUI/Panels/Properties/propertiesPanel.hpp
@@ -10,14 +10,9 @@
 
 /*
  *
- * TODO: Decide whether or not creating a custom QDockWidget is the correct choice.
- * The QDockWidget may not need to be customized. However, the widgets inside of
- * them are going to need to be customized. Including the sliders.
- *
- * I need to look at the differences between the volumes and determine how those are
- * loaded in. I believe that a tab will need to appear no matter what since there is
- * always one renderview. However once it is loaded in then the tabs will need to be
- * populated with either a volume or mesh properties.
+ * TODO: See if there is a way to inherit from a QMainWindow to use QDockWidgets
+ *       instead. These tab widgets are not undockable so there is no way to
+ *       take them off the dock.
  *
 */
 class PropertiesPanel : public QWidget
