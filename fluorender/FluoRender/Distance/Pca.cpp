f0a43a3e5c1f93c015f3fa098de607117c2c5373
basisunus <wanyong@cs.utah.edu>
2019-12-30 19:35:09 -0700

    initialize cov matrix

--- fluorender/FluoRender/Distance/Pca.cpp
+++ fluorender/FluoRender/Distance/Pca.cpp
@@ -76,6 +76,7 @@ void Pca::Compute(bool upd_cov)
 		for (int i = 0; i < 3; ++i)
 		for (int j = 0; j < 3; ++j)
 		{
+			m_cov[i][j] = 0;
 			for (int n = 0; n < N; ++n)
 				m_cov[i][j] += (points[n])(i) * (points[n])(j);
 		}
