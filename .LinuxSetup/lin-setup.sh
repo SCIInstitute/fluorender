c5e5d532750f0e272b64e6f85a973b59cee324f9
--global <--global>
2020-03-21 00:17:00 -0600

    Finally compiled. Threaded the make process so it will compile quicker. Will work on Windows version on Monday.

--- .LinuxSetup/lin-setup.sh
+++ .LinuxSetup/lin-setup.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+# Get the version of the linux distro
 version=$(lsb_release -sr)
 
+# update the repos and install build-essential
 sudo apt update
 sudo apt install build-essential
 
@@ -10,11 +12,13 @@ sudo apt install libxkbcommon-x11-0
 sudo apt install gcc-8
 sudo apt install g++-8
 
+# make gcc-8 the default 
 sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-7 700 --slave /usr/bin/g++ g++ /usr/bin/g++-7
 sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-8 800 --slave /usr/bin/g++ g++ /usr/bin/g++-8
 
 sudo apt install openjdk
 
+# check which nasm to install
 case $version in
 16.04)
   echo "Version 16.04 detected. Installing nasm by source."
@@ -32,6 +36,7 @@ case $version in
   sudo apt install nasm
 esac
 
+# OpenGL dependencies
 sudo apt install mesa-common-dev
 sudo apt install libgl1-mesa-dev
 sudo apt install mesa-utils-extra
@@ -39,12 +44,13 @@ sudo apt install libglapi-mesa
 sudo apt install libglu1-mesa-dev
 sudo apt install freeglut3-dev
 
+# OpenCL dependencies
 sudo apt install ocl-icd-libopencl1
 sudo apt install ocl-icd-opencl-dev
 sudo apt install opencl-headers
 
+# incstall Qt via the beiner package
 sudo apt install qtcreator
-
 case $version in
 16.04)
   echo "Version 16.04 detected. Installing Qt 5.12 for Xenial."
