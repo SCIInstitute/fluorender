9de0a24defb4ce1511c7425f43ddda8d092cf8f9
--global <--global>
2020-03-23 12:45:50 -0600

    Testing.

--- .WinSetup/winSetup.ps1
+++ .WinSetup/winSetup.ps1
@@ -1,2 +1,9 @@
-Write-Host "I did a thing."
+Write-Host "Setup for Windows"
 
+Write-Host "Installing Qt Creator..."
+choco install qtcreator
+
+Write-Host "Installing OpenCL..."
+choco install opencl-intel-cpu-runtime
+
+Write-Host "Done."
