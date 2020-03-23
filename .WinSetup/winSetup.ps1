aaf26f119d08c96cff0bfc86f9fb21034885f019
--global <--global>
2020-03-23 15:58:55 -0600

    Trying this manual download of OpenCL.

--- .WinSetup/winSetup.ps1
+++ .WinSetup/winSetup.ps1
@@ -3,7 +3,22 @@ Write-Host "Setup for Windows"
 Write-Host "Installing Qt Creator..."
 choco install qtcreator
 
-Write-Host "Installing OpenCL..."
-choco install opencl-intel-cpu-runtime
+mkdir OpenCL
+cd OpenCL
+Write-Host "Downloading the OpenCL Headers..."
+git clone --branch v2020.03.13 https://github.com/KhronosGroup/OpenCL-Headers
+Write-Host "Done, cloning into the Main OpenCL library."
+git clone --branch v2020.03.13 https://github.com/KhronosGroup/OpenCL-ICD-Loader
+Write-Host "Done, creating CL dir in Loader and moving files over..."
+mkdir OpenCL-ICD-Loader/inc/CL
+mv OpenCL-Headers/CL/*.h OpenCL-ICD-Loader/inc/CL
+ls OpenCL-ICD-Loader/inc/CL
+Write-Host "Done. Creating lib..."
+
+cd OpenCL-ICD-Loader ; mkdir lib ; cd lib
+cmake ..
+cmake --build
 
 Write-Host "Done."
+
+
