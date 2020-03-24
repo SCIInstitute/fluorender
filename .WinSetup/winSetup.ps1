a12ea535843946b6c4c2827e6ef7f10be955429b
--global <--global>
2020-03-24 11:21:32 -0600

    Added a proper install Qt step.

--- .WinSetup/winSetup.ps1
+++ .WinSetup/winSetup.ps1
@@ -1,6 +1,19 @@
-Write-Host "Setup for Windows"
+Write-Host "Building OpenCL..."
 
-Write-Host "Installing Qt Creator..."
-choco install qtcreator
+mkdir OpenCL
+
+Move-Item OpenCL-Headers OpenCL
+Move-Item OpenCL-ICD-Loader OpenCL
+cd OpenCL
+
+mkdir OpenCL-ICD-Loader/inc/CL
+
+mv OpenCL-Headers/CL/*.h OpenCL-ICD-Loader/inc/CL
+
+mkdir lib ; cd lib
+cmake ..\OpenCL-ICD-Loader
+cmake --build . --config Release
 
 Write-Host "Done."
+
+ls ; cd .. ; ls ; cd .. ; ls
