1dd63d6d5d267f5514eca5b304126184d98fac3f
basisunus <wanyong@cs.utah.edu>
2020-04-22 16:07:31 -0600

    started reading nd2 files

--- fluorender/FluoRender/Formats/nd2_reader.cpp
+++ fluorender/FluoRender/Formats/nd2_reader.cpp
@@ -26,6 +26,7 @@ FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
 DEALINGS IN THE SOFTWARE.
 */
 #include "nd2_reader.h"
+#include <json.hpp>
 #include "../compatibility.h"
 #include <stdio.h>
 
@@ -89,12 +90,37 @@ int ND2Reader::Preprocess()
 		return READER_OPEN_FAIL;
 	}
 
-	LIMCHAR buffer[ND2_STR_SIZE];
+	//get attributes
+	LIMSTR attstr = Lim_FileGetAttributes(h);
+	nlohmann::json j = nlohmann::json::parse(attstr);
+	Lim_FileFreeString(attstr);
 	std::string str;
-	m_nd2_info.init();
+	for (nlohmann::json::iterator it = j.begin(); it != j.end(); ++it)
+	{
+		str = it.key();
+		if (str == "bitsPerComponentInMemory")
+			m_bits = it.value();
+		else if (str == "bitsPerComponentSignificant")
+			m_bits_used = it.value();
+		else if (str == "componentCount")
+			m_chan_num = it.value();
+		else if (str == "widthPx")
+			m_x_size = it.value();
+		else if (str == "heightPx")
+			m_y_size = it.value();
+	}
+
+	if (m_bits > 8)
+	{
+		m_max_value = std::pow(2.0, double(m_bits_used));
+		m_scalar_scale = 65535.0 / m_max_value;
+	}
+
+	LIMCHAR buffer[ND2_STR_SIZE];
 	LIMSIZE coordsize = Lim_FileGetCoordSize(h);
 	LIMUINT fnum = Lim_FileGetSeqCount(h);
 	int ti = -1, zi = -1, xyi = -1;//indecis in coords
+	int maxz = 0;
 	std::vector<LIMUINT> vec(coordsize);
 	if (coordsize > 0)
 	{
@@ -118,6 +144,7 @@ int ND2Reader::Preprocess()
 			frame.slice = zi >= 0 ? vec[zi] : 0;
 			frame.seq = i;
 			AddFrameInfo(frame);
+			maxz = frame.slice > maxz ? frame.slice : maxz;
 		}
 	}
 	else
@@ -134,6 +161,11 @@ int ND2Reader::Preprocess()
 		}
 	}
 	Lim_FileClose(h);
+
+	m_time_num = m_nd2_info.times.size();
+	m_slice_num = maxz + 1;
+	m_cur_time = 0;
+	m_data_name = GET_NAME(m_path_name);
 #endif
 	return READER_OK;
 }
@@ -219,6 +251,59 @@ double ND2Reader::GetExcitationWavelength(int chan)
 Nrrd* ND2Reader::Convert(int t, int c, bool get_max)
 {
 	Nrrd *data = 0;
+#ifndef _DEBUG
+	LIMCWSTR filename = m_path_name.c_str();
+	LIMFILEHANDLE h = Lim_FileOpenForRead(filename);
+	if (h == nullptr)
+	{
+		Lim_FileClose(h);
+		return 0;
+	}
+
+	if (t >= 0 && t < m_time_num &&
+		c >= 0 && c < m_chan_num &&
+		m_slice_num > 0 &&
+		m_x_size > 0 &&
+		m_y_size > 0)
+	{
+		switch (m_bits)
+		{
+		case 8:
+		{
+			unsigned long long mem_size = (unsigned long long)m_x_size*
+				(unsigned long long)m_y_size*(unsigned long long)m_slice_num;
+			unsigned char *val = new (std::nothrow) unsigned char[mem_size];
+			ReadChannel(h, t, c, val);
+			//create nrrd
+			data = nrrdNew();
+			nrrdWrap(data, val, nrrdTypeUChar, 3, (size_t)m_x_size, (size_t)m_y_size, (size_t)m_slice_num);
+			nrrdAxisInfoSet(data, nrrdAxisInfoSpacing, m_xspc, m_yspc, m_zspc);
+			nrrdAxisInfoSet(data, nrrdAxisInfoMax, m_xspc*m_x_size, m_yspc*m_y_size, m_zspc*m_slice_num);
+			nrrdAxisInfoSet(data, nrrdAxisInfoMin, 0.0, 0.0, 0.0);
+			nrrdAxisInfoSet(data, nrrdAxisInfoSize, (size_t)m_x_size, (size_t)m_y_size, (size_t)m_slice_num);
+		}
+		break;
+		case 16:
+		{
+			unsigned long long mem_size = (unsigned long long)m_x_size*
+				(unsigned long long)m_y_size*(unsigned long long)m_slice_num;
+			unsigned short *val = new (std::nothrow) unsigned short[mem_size];
+			ReadChannel(h, t, c, val);
+			//create nrrd
+			data = nrrdNew();
+			nrrdWrap(data, val, nrrdTypeUShort, 3, (size_t)m_x_size, (size_t)m_y_size, (size_t)m_slice_num);
+			nrrdAxisInfoSet(data, nrrdAxisInfoSpacing, m_xspc, m_yspc, m_zspc);
+			nrrdAxisInfoSet(data, nrrdAxisInfoMax, m_xspc*m_x_size, m_yspc*m_y_size, m_zspc*m_slice_num);
+			nrrdAxisInfoSet(data, nrrdAxisInfoMin, 0.0, 0.0, 0.0);
+			nrrdAxisInfoSet(data, nrrdAxisInfoSize, (size_t)m_x_size, (size_t)m_y_size, (size_t)m_slice_num);
+		}
+		break;
+		}
+	}
+
+	Lim_FileClose(h);
+	m_cur_time = t;
+#endif
 	return data;
 }
 
@@ -276,4 +361,37 @@ void ND2Reader::AddFrameInfo(FrameInfo &frame)
 		chaninfo = &(timeinfo->channels[chan]);
 
 	chaninfo->chann.push_back(frame);
+}
+
+bool ND2Reader::ReadChannel(LIMFILEHANDLE h, int t, int c, void* val)
+{
+	ChannelInfo* cinfo = GetChaninfo(t, 0);
+	if (!cinfo)
+		return false;
+
+	unsigned long long xysize = (unsigned long long)m_x_size * m_y_size;
+	unsigned long long pos;
+	int bytes = m_bits / 8;
+	unsigned char *dst, *src;
+	for (size_t i = 0; i < cinfo->chann.size(); ++i)
+	{
+		int seq = cinfo->chann[i].seq;
+		int z = cinfo->chann[i].slice;
+		LIMPICTURE pic = { 0 };
+		Lim_FileGetImageData(h, seq, &pic);
+		pos = xysize * z;//consider it a brick
+		for (unsigned int j = 0; j < pic.uiHeight; ++j)
+		for (unsigned int k = 0; k < pic.uiWidth; ++k)
+		{
+			dst = (unsigned char*)val;
+			dst += (pos + m_x_size * j + k) * bytes;
+			src = (unsigned char*)(pic.pImageData);
+			src += pic.uiWidthBytes * j + k * pic.uiComponents * bytes + c * bytes;
+			memcpy(dst, src, bytes);
+		}
+
+		Lim_DestroyPicture(&pic);
+	}
+
+	return true;
 }
