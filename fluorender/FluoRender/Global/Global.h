4e92641bfba0fd9e0377ba0e0a4bc9cecbae5f55
basisunus <basisunus@yahoo.com>
2021-12-20 12:49:26 -0700

    adding global and volumedata

---
+++ fluorender/FluoRender/Global/Global.h
@@ -0,0 +1,141 @@
+/*
+For more information, please see: http://software.sci.utah.edu
+
+The MIT License
+
+Copyright (c) 2018 Scientific Computing and Imaging Institute,
+University of Utah.
+
+
+Permission is hereby granted, free of charge, to any person obtaining a
+copy of this software and associated documentation files (the "Software"),
+to deal in the Software without restriction, including without limitation
+the rights to use, copy, modify, merge, publish, distribute, sublicense,
+and/or sell copies of the Software, and to permit persons to whom the
+Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included
+in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+DEALINGS IN THE SOFTWARE.
+*/
+#ifndef _GLOBAL_H_
+#define _GLOBAL_H_
+
+#include <Scenegraph/VolumeFactory.h>
+#include <Scenegraph/MeshFactory.h>
+#include <Scenegraph/AnnotationFactory.h>
+#include <Fui/AgentFactory.h>
+#include <Fui/IconList.h>
+#include <Processor/ProcessorFactory.h>
+
+namespace FL
+{
+	//automatically creates the factories and provide global access
+	class Global
+	{
+	public:
+		static Global& instance() { return instance_; }
+
+		inline size_t getNum()
+		{
+			size_t volume_num = volume_factory_->getNum();
+			size_t mesh_num = mesh_factory_->getNum();
+			size_t annotations_num = annotation_factory_->getNum();
+
+			return volume_num + mesh_num + annotations_num;
+		}
+
+		inline Object* get(size_t i)
+		{
+			size_t volume_num = volume_factory_->getNum();
+			size_t mesh_num = mesh_factory_->getNum();
+			size_t annotations_num = annotation_factory_->getNum();
+
+			if (i < volume_num)
+				return volume_factory_->get(i);
+			else if (i < volume_num + mesh_num)
+				return mesh_factory_->get(i - volume_num);
+			else if (i < volume_num + mesh_num + annotations_num)
+				return annotation_factory_->get(i - volume_num - mesh_num);
+			return 0;
+		}
+
+		inline size_t getIndex(const Object* object) const
+		{
+			size_t volume_num = volume_factory_->getNum();
+			size_t mesh_num = mesh_factory_->getNum();
+			size_t annotations_num = annotation_factory_->getNum();
+
+			for (size_t i = 0; i < volume_num; ++i)
+			{
+				if (volume_factory_->get(i) == object)
+					return i;
+			}
+			for (size_t i = 0; i < mesh_num; ++i)
+			{
+				if (mesh_factory_->get(i) == object)
+					return i + volume_num;
+			}
+			for (size_t i = 0; i < annotations_num; ++i)
+			{
+				if (annotation_factory_->get(i) == object)
+					return i + volume_num + mesh_num;
+			}
+			return volume_num + mesh_num + annotations_num;
+		}
+
+		VolumeFactory& getVolumeFactory()
+		{ return *volume_factory_; }
+
+		MeshFactory& getMeshFactory()
+		{ return *mesh_factory_; }
+
+		AnnotationFactory& getAnnotationFactory()
+		{ return *annotation_factory_;}
+
+		FUI::AgentFactory& getAgentFactory()
+		{ return *agent_factory_; }
+
+		FUI::IconList& getIconList(bool shown)
+		{ return shown?shown_icon_list_:hidden_icon_list_; }
+
+		void addListObserver(Observer* obsrvr)
+		{
+			volume_factory_->addObserver(obsrvr);
+			mesh_factory_->addObserver(obsrvr);
+			annotation_factory_->addObserver(obsrvr);
+		}
+
+		ProcessorFactory& getProcessorFactory()
+		{ return *processor_factory_; }
+
+	private:
+		Global();
+
+		static Global instance_;
+
+		//objects in these will be shown in the list panel
+		ref_ptr<VolumeFactory> volume_factory_;
+		ref_ptr<MeshFactory> mesh_factory_;
+		ref_ptr<AnnotationFactory> annotation_factory_;
+
+		//list of agent
+		ref_ptr<FUI::AgentFactory> agent_factory_;
+
+		//list of processors
+		ref_ptr<ProcessorFactory> processor_factory_;
+
+		//icon list
+		FUI::IconList shown_icon_list_;
+		FUI::IconList hidden_icon_list_;
+	};
+}
+
+#endif
---
+++ fluorender/FluoRender/Scenegraph/VolumeGroup.cpp
@@ -0,0 +1,165 @@
+/*
+For more information, please see: http://software.sci.utah.edu
+
+The MIT License
+
+Copyright (c) 2018 Scientific Computing and Imaging Institute,
+University of Utah.
+
+
+Permission is hereby granted, free of charge, to any person obtaining a
+copy of this software and associated documentation files (the "Software"),
+to deal in the Software without restriction, including without limitation
+the rights to use, copy, modify, merge, publish, distribute, sublicense,
+and/or sell copies of the Software, and to permit persons to whom the
+Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included
+in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+DEALINGS IN THE SOFTWARE.
+*/
+
+#include <Scenegraph/VolumeGroup.h>
+#include <Scenegraph/VolumeData.h>
+
+using namespace FL;
+
+VolumeGroup::VolumeGroup()
+{
+
+}
+
+VolumeGroup::VolumeGroup(const VolumeGroup& group, const CopyOp& copyop) :
+	Group(group, copyop)
+{
+
+}
+
+VolumeGroup::VolumeGroup(const VolumeData& vd, const CopyOp& copyop) :
+	Group()
+{
+	copyValues(vd, copyop);
+}
+
+VolumeGroup::~VolumeGroup()
+{
+
+}
+
+bool VolumeGroup::insertChild(size_t index, Node* child)
+{
+	if (child)
+	{
+		std::string ss[] = {
+			"gamma r",
+			"gamma g",
+			"gamma b",
+			"brightness r",
+			"brightness g",
+			"brightness b",
+			"equalize r",
+			"equalize g",
+			"equalize b",
+			"sync r",
+			"sync g",
+			"sync b"
+		};
+		ValueCollection names(std::begin(ss), std::end(ss));
+		syncValues(names, child);
+		child->syncValues(names, this);
+	}
+	return Group::insertChild(index, child);
+}
+
+bool VolumeGroup::removeChildren(size_t pos, size_t num)
+{
+	if (pos < m_children.size() && num > 0)
+	{
+		size_t end = pos + num;
+		if (end > m_children.size())
+			end = m_children.size();
+
+		std::string ss[] = {
+			"gamma r",
+			"gamma g",
+			"gamma b",
+			"brightness r",
+			"brightness g",
+			"brightness b",
+			"equalize r",
+			"equalize g",
+			"equalize b",
+			"sync r",
+			"sync g",
+			"sync b"
+		};
+		ValueCollection names(std::begin(ss), std::end(ss));
+
+		for (unsigned int i = pos; i < end; ++i)
+		{
+			Node* child = m_children[i].get();
+			if (child)
+			{
+				unsyncValues(names, child);
+				child->unsyncValues(names, this);
+			}
+		}
+	}
+	return Group::removeChildren(pos, num);
+}
+
+bool VolumeGroup::setChild(size_t i, Node* node)
+{
+	if (i < m_children.size() && node)
+	{
+		std::string ss[] = {
+			"gamma r",
+			"gamma g",
+			"gamma b",
+			"brightness r",
+			"brightness g",
+			"brightness b",
+			"equalize r",
+			"equalize g",
+			"equalize b",
+			"sync r",
+			"sync g",
+			"sync b"
+		};
+		ValueCollection names(std::begin(ss), std::end(ss));
+
+		Node* orig_node = m_children[i].get();
+		if (orig_node)
+		{
+			unsyncValues(names, orig_node);
+			orig_node->unsyncValues(names, this);
+		}
+
+		syncValues(names, node);
+		node->syncValues(names, this);
+	}
+	return Group::setChild(i, node);
+}
+
+void VolumeGroup::OnRandomizeColor(Event& event)
+{
+	//maybe there is a better solution?
+	Node* node = dynamic_cast<Node*>(event.penultimate());
+	if (node && containsNode(node))
+		return;
+
+	for (auto it = m_children.begin();
+		it != m_children.end(); ++it)
+	{
+		bool bval;
+		(*it)->toggleValue("randomize color", bval, event);
+	}
+}
+
