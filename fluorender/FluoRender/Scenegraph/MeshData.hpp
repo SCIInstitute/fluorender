2a227ed262aedb7e6d72dabd6ebe3f2e6596cccb
basisunus <wanyong@cs.utah.edu>
2022-05-02 17:12:50 -0600

    moved definitions to headers of scenegraph classes

--- fluorender/FluoRender/Scenegraph/MeshData.hpp
+++ fluorender/FluoRender/Scenegraph/MeshData.hpp
@@ -33,6 +33,64 @@ DEALINGS IN THE SOFTWARE.
 #include <Names.hpp>
 #include <glm/glm.hpp>
 
+//file properties
+#define gstDataPath "data path"
+//bounding box
+#define gstBounds "bounds"
+#define gstBoundsTf "bounds tf"//bounding box after transformation
+#define gstCenter "center"
+//clipping planes
+#define gstClipPlanes "clip planes"
+#define gstResX "res x"
+#define gstResY "res y"
+#define gstResZ "res z"
+#define gstClipDistX "clip dist x"
+#define gstClipDistY "clip dist y"
+#define gstClipDistZ "clip dist z"
+//display
+#define gstDisplay "display"
+#define gstDrawBounds "draw bounds"
+//lighting
+#define gstShadingEnable "shding enable"
+//depth atten
+#define gstDepthAtten "depth atten"
+#define gstDaInt "da int"
+#define gstDaStart "da start"
+#define gstDaEnd "da end"
+//material
+#define gstColor "color"
+#define gstMatAmb "mat amb"
+#define gstMatDiff "mat diff"
+#define gstMatSpec "mat spec"
+#define gstMatShine "mat shine"
+#define gstAlpha "alpha"
+#define gstRandomizeColor "randomize color"
+//shadow
+#define gstShadowEnable "shadow enable"
+#define gstShadowInt "shadow int"
+#define gstShadowDirX "shadow dir x"
+#define gstShadowDirY "shadow dir y"
+//size limiter
+#define gstLimitEnable "limit enable"//size limiter
+#define gstLimit "limit"
+//transformation
+#define gstTransX "trans x"
+#define gstTransY "trans y"
+#define gstTransZ "trans z"
+#define gstRotX "rot x"
+#define gstRotY "rot y"
+#define gstRotZ "rot z"
+#define gstScaleX "scale x"
+#define gstScaleY "scale y"
+#define gstScaleZ "scale z"
+//legend
+#define gstLegend "legend"
+#define gstViewport "viewport"
+//sync group
+#define gstSyncGroup "sync group"
+//selection
+#define gstSelected "selected"
+
 struct _GLMmodel;
 typedef _GLMmodel GLMmodel;
 namespace flvr
