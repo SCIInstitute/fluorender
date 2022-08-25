cadea9f9f08c7cfd3008c92606c77f34c818b715
ARSanderson <allen@sci.utah.edu>
2022-08-25 12:58:46 -0600

    cleanup of parsing

--- Src/FluoRender/Main/Main.cpp
+++ Src/FluoRender/Main/Main.cpp
@@ -25,67 +25,80 @@ int main(int argc, char *argv[])
     QSurfaceFormat::setDefaultFormat(format);
 
     QApplication app(argc, argv);
-    app.setWindowIcon(QIcon(":FluoRender/FluoRender/UI/icons/FluoRender_icon.gif"));
+    app.setWindowIcon(QIcon(":FluoRender/FluoRender/UI/icons/FluoRenderIcon.gif"));
 
+    // Parse any command line arguments.
     QCommandLineParser parser;
     parser.setApplicationDescription(QCoreApplication::applicationName());
     parser.addHelpOption();
     parser.addVersionOption();
 
-    parser.addPositionalArgument("[file|project]", QCoreApplication::translate("main", "An image file or project to load."));
+    // Optional arguments at the end is a file name.
+    parser.addPositionalArgument("[file|project]", QCoreApplication::translate("main", "Image files or project to load."));
 
     QCommandLineOption fullscreenkCLO(QStringList() << "fs" << "fullscreen",
                                        QCoreApplication::translate("main", "Start FluoRender in full screen."));
     parser.addOption(fullscreenkCLO);
 
-    QCommandLineOption benchmarkCLO(QStringList() << "b" << "benchmark",
-                                       QCoreApplication::translate("main", "Run FluoRender in the benchmark mode."));
-    parser.addOption(benchmarkCLO);
+    // QCommandLineOption benchmarkCLO(QStringList() << "b" << "benchmark",
+    //                                    QCoreApplication::translate("main", "Run FluoRender in the benchmark mode."));
+    // parser.addOption(benchmarkCLO);
 
-    QCommandLineOption imagejCLO(QStringList() << "j" << "imagej",
-                                       QCoreApplication::translate("main", "Use imagej to load volume files."));
-    parser.addOption(imagejCLO);
+    // QCommandLineOption imagejCLO(QStringList() << "j" << "imagej",
+    //                                    QCoreApplication::translate("main", "Use imagej to load volume files."));
+    // parser.addOption(imagejCLO);
 
-    QCommandLineOption movieCLO(QStringList() << "m" << "movie",
-                                       QCoreApplication::translate("main", "Export a movie."),
-                                       QCoreApplication::translate("main", "filename"), "fluorender");
-    parser.addOption(movieCLO);
+    // Movie options
+    // QCommandLineOption movieCLO(QStringList() << "m" << "movie",
+    //                                    QCoreApplication::translate("main", "Export a movie."),
+    //                                    QCoreApplication::translate("main", "filename"), "fluorender");
+    // parser.addOption(movieCLO);
 
-    QCommandLineOption alphaCLO(QStringList() << "a" << "alpha",
-                                       QCoreApplication::translate("main", "Save the alpha channel of the exported movie."));
-    parser.addOption(alphaCLO);
+    // QCommandLineOption alphaCLO(QStringList() << "a" << "alpha",
+    //                                    QCoreApplication::translate("main", "Save the alpha channel of the exported movie."));
+    // parser.addOption(alphaCLO);
 
-    QCommandLineOption floatCLO(QStringList() << "f" << "float",
-                                       QCoreApplication::translate("main", "Save the float channel of the exported movie."));
-    parser.addOption(floatCLO);
+    // QCommandLineOption floatCLO(QStringList() << "f" << "float",
+    //                                    QCoreApplication::translate("main", "Save the float channel of the exported movie."));
+    // parser.addOption(floatCLO);
 
-    QCommandLineOption bitrateCLO(QStringList() << "br" << "bitrate",
-                                       QCoreApplication::translate("main", "Bit rate (Mbps) of the exported movie."),
-                                       QCoreApplication::translate("main", "bitrate"), "10");
-    parser.addOption(bitrateCLO);
+    // QCommandLineOption bitrateCLO(QStringList() << "br" << "bitrate",
+    //                                    QCoreApplication::translate("main", "Bit rate (Mbps) of the exported movie."),
+    //                                    QCoreApplication::translate("main", "bitrate"), "10");
+    // parser.addOption(bitrateCLO);
 
-    QCommandLineOption lzwCLO(QStringList() << "c" << "compress",
-                                       QCoreApplication::translate("main", "Compress the exported movie using LZW."));
-    parser.addOption(lzwCLO);
+    // QCommandLineOption lzwCLO(QStringList() << "c" << "compress",
+    //                                    QCoreApplication::translate("main", "Compress the exported movie using LZW."));
+    // parser.addOption(lzwCLO);
 
-
+    // Do the actual parsing.
     parser.process(app);
 
-    bool benchmark = parser.isSet(benchmarkCLO);
+    // bool benchmark = parser.isSet(benchmarkCLO);
+    // bool imagej    = parser.isSet(imagejCLO);
 
-    std::cerr << "benchmark  " << benchmark << std::endl;
+    // QString filename  = parser.value(movieCLO);
+    // bool alphaChannel = parser.isSet(alphaCLO);
+    // bool floatChannel = parser.isSet(floatCLO);
+    // double bitrate    = parser.value(bitrateCLO).toDouble();
+    // bool compress     = parser.isSet(lzwCLO);
 
+    // Create the main window.
     QvisMainWindow mainWin;
 
+    // Optional argument is a file name.
     if (!parser.positionalArguments().isEmpty())
     {
         const QStringList args = parser.positionalArguments();
-        QString filename = args.at(0);
 
-        if(!filename.isEmpty())
-            mainWin.loadFile(filename);
+        for(const auto & filename : args)
+        {
+          if(!filename.isEmpty())
+                mainWin.loadFile(filename);
+        }
     }
 
+    // Raise the window.
     if(parser.isSet(fullscreenkCLO))
         mainWin.showFullScreen();
     else
