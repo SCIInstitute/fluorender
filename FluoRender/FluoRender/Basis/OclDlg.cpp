6f9c2cd442a8a1947fefa530d88058331740515f
basisunus <basisunus@yahoo.com>
2023-07-20 12:48:32 -0600

    ocl dialog update

--- FluoRender/FluoRender/Basis/OclDlg.cpp
+++ FluoRender/FluoRender/Basis/OclDlg.cpp
@@ -56,7 +56,7 @@ m_view(0)
 {
 	// temporarily block events during constructor:
 	wxEventBlocker blocker(this);
-	SetDoubleBuffered(true);
+	//SetDoubleBuffered(true);
 
 	//validator: integer
 	wxIntegerValidator<unsigned int> vald_int;
