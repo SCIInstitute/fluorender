b700d754b694b929c95ba6885f0193da14f7d2e4
basisunus <basisunus@yahoo.com>
2024-05-07 16:30:49 -0600

    adding lg renderer

---
+++ FluoRender/FluoRender/LookingGlass/LookingGlassRenderer.cpp
@@ -0,0 +1,57 @@
+/*
+For more information, please see: http://software.sci.utah.edu
+
+The MIT License
+
+Copyright (c) 2024 Scientific Computing and Imaging Institute,
+University of Utah.
+
+
+Permission is hereby granted, free of charge, to any person obtaining a
+copy of this software and associated documentation files (the "Software"),
+to deal in the Software without restriction, including without limitation
+the rights to use, copy, modify, merge, publish, distribute, sublicense,
+and/or sell copies of the Software, and to permit persons to whom the
+Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included
+in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+DEALINGS IN THE SOFTWARE.
+*/
+
+#include <LookingGlassRenderer.h>
+#include <HoloPlayCore.h>
+
+LookingGlassRenderer::LookingGlassRenderer() :
+	m_initialized(false)
+{
+
+}
+
+LookingGlassRenderer::~LookingGlassRenderer()
+{
+	Close();
+}
+
+void LookingGlassRenderer::Init()
+{
+	if (m_initialized)
+		return;
+	hpc_client_error errco = hpc_InitializeApp("FluoRender", hpc_LICENSE_NONCOMMERCIAL);
+}
+
+void LookingGlassRenderer::Close()
+{
+	if (m_initialized)
+	{
+		hpc_CloseApp();
+		m_initialized = false;
+	}
+}
