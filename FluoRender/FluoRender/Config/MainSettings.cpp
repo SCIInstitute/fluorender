ff413bcec27140cfaeccad297f2111070d640fd3
wanyong <wanyong@cs.utah.edu>
2024-12-31 15:52:39 -0700

    updated glew

--- FluoRender/FluoRender/Config/MainSettings.cpp
+++ FluoRender/FluoRender/Config/MainSettings.cpp
@@ -26,7 +26,7 @@ FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
 DEALINGS IN THE SOFTWARE.
 */
 
-#include <glew.h>
+#include <GL/glew.h>
 #include <MainSettings.h>
 #include <Names.h>
 #include <compatibility.h>
